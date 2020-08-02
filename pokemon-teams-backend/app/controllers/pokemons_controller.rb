8c51be7d5aa0bd1039c1e4d0c76cf1fb87c2f306
github username <lushiyun@outlook.com>
2020-08-02 17:17:01 -0400

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,13 @@
 class PokemonsController < ApplicationController
+  def create
+    trainer = Trainer.find_by(id: params[:trainer_id])
+    pokemon = Pokemon.create(nickname: Faker::Name.first_name, species: Faker::Games::Pokemon.name, trainer: trainer)
+    render json: PokemonSerializer.new(pokemon).to_serialized_json
+  end
+
+  def destroy
+    pokemon = Pokemon.find_by(id: params[:pokemon_id])
+    render json: PokemonSerializer.new(pokemon).to_serialized_json
+    pokemon.destroy
+  end
 end
--- pokemon-teams-backend/app/serializers/trainer_serializer.rb
+++
@@ -1,4 +0,0 @@
-class TrainerSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes 
-end
